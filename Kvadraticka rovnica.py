0a5ff7baaa4d58bdeb45d1280dd0467bc796e53f
Kolbynko <90548034+Kolbynko@users.noreply.github.com>
2021-09-28 09:34:38 +0200

    Add files via upload

--- Kvadraticka rovnica.py
+++ Kvadraticka rovnica.py
@@ -1,4 +1,5 @@
 import math
+
 a=int(input('Zadaj hodnotu pre a: '))
 b=int(input('Zadaj hodnotu pre b: '))
 c=int(input('Zadaj hodnotu pre c: '))
@@ -7,7 +8,5 @@ d=(b*b)-(4*a*c)
 if d < 0:
     print('Rovnica nemá riešenie')
 else:
-    x=((-b)+math.sqrt(d))/(2*a)
-    y=((-b)-math.sqrt(d))/(2*a)
-
-    print('x1 je',x, 'a x2 je',y )
+    print('x1 je',int((-b)+math.sqrt(d)/(2*a)))
+    print('x2 je',int((-b)-math.sqrt(d)/(2*a)))
